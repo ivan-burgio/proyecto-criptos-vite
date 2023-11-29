c500cad35c0ce86af38b2f8a4b3d13757c433772
ivan-burgio <ivan.24.burgio@gmail.com>
2023-11-29 10:37:09 -0300

    Styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react';
+import styled from '@emotion/styled';
+
+const Heading = styled.h1 `
+    font-family: 'Lato', sans-serif;
+    color: #FFF;
+`
 
 function App() {
 
   return (
     <>
-        <h1>Desde app</h1>
+        <Heading>Desde app</Heading>
     </>
   )
 }
