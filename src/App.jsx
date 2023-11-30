57c2b2736064e4347851004ba940e9b4690b99f7
ivan-burgio <ivan.24.burgio@gmail.com>
2023-11-30 13:13:21 -0300

    Creando el input de formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import styled from '@emotion/styled';
+import Formulario from './components/Formulario';
 import ImagenCripto from './img/imagen-criptos.png'
 
 const Contenedor = styled.div `
@@ -50,6 +51,7 @@ function App() {
         />
         <div>
             <Heading>Cotiza Criptomonedas al Instante</Heading>
+            <Formulario />
         </div>
     </Contenedor>
   )
